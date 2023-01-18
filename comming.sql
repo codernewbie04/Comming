e91887719f66ba9154480104c66834f5836511be
Akmal Muhamad Firdaus <coder.newbie04@gmail.com>
2023-01-18 16:08:31 +0700

    Add preview

--- comming.sql
+++ comming.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2022 at 03:54 AM
+-- Generation Time: Jan 18, 2023 at 10:04 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
---
+++ src/com/duo_sio/comming/controller/TestTist.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.duo_sio.comming.controller;
+
+/**
+ *
+ * @author DUO SIO
+ */
+public class TestTist {
+    public boolean test(int num) {
+        return num == 1;
+    }
+}
